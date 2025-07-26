8f10dea6ff2ad6469252b915f138f38543e41f22
Caio Olubuni <caiocsn@hotmail.com>
2025-07-26 20:00:13 -0300

    Melhoria tratamento CPV

--- Parte 1 - SQL/tratamento_bases_cpv.sql
+++ Parte 1 - SQL/tratamento_bases_cpv.sql
@@ -6,24 +6,27 @@ BEGIN
         SAFE_CAST(ano AS INT64) AS ano,
         UPPER(cod_un_negocio) AS cod_un_negocio,
         LPAD(SAFE_CAST(cod_material AS STRING), 5, '0') AS cod_material,
-        SAFE_CAST(cpv AS FLOAT64) AS cpv
+        MIN(SAFE_CAST(cpv AS FLOAT64)) AS cpv
       FROM tb_cpv_historico
+      GROUP BY 1, 2, 3
     ),
     base_cpv_ecomm AS (
       SELECT
         SAFE_CAST(ano AS INT64) AS ano,
         UPPER(cod_un_negocio) AS cod_un_negocio,
         LPAD(SAFE_CAST(cod_material AS STRING), 5, '0') AS cod_material,
-        SAFE_CAST(cpv AS FLOAT64) AS cpv
+        MIN(SAFE_CAST(cpv AS FLOAT64)) AS cpv
       FROM tb_cpv_ecomm
+      GROUP BY 1, 2, 3
     ),
     base_cpv_linx AS (
       SELECT
         SAFE_CAST(ano AS INT64) AS ano,
         UPPER(cod_un_negocio) AS cod_un_negocio,
         LPAD(SAFE_CAST(cod_material AS STRING), 5, '0') AS cod_material,
-        SAFE_CAST(cpv AS FLOAT64) AS cpv
+        MIN(SAFE_CAST(cpv AS FLOAT64)) AS cpv
       FROM tb_cpv_ecomm_linx
+      GROUP BY 1, 2, 3
     ),
     indices_material_ano AS (
       SELECT ano, cod_un_negocio, cod_material FROM base_cpv_historico
@@ -40,22 +43,29 @@ BEGIN
         COALESCE(b_hist.cpv, b_ecomm.cpv, b_linx.cpv) AS cpv
       FROM indices_material_ano AS i
       LEFT JOIN base_cpv_historico AS b_hist
-        ON i.cod_material = b_hist.cod_material AND i.ano = b_hist.ano AND i.cod_un_negocio = b_hist.cod_un_negocio
+        ON i.cod_material = b_hist.cod_material 
+        AND i.ano = b_hist.ano 
+        AND i.cod_un_negocio = b_hist.cod_un_negocio
       LEFT JOIN base_cpv_ecomm AS b_ecomm
-        ON i.cod_material = b_ecomm.cod_material AND i.ano = b_ecomm.ano AND i.cod_un_negocio = b_ecomm.cod_un_negocio
+        ON i.cod_material = b_ecomm.cod_material 
+        AND i.ano = b_ecomm.ano 
+        AND i.cod_un_negocio = b_ecomm.cod_un_negocio
       LEFT JOIN base_cpv_linx AS b_linx
-        ON i.cod_material = b_linx.cod_material AND i.ano = b_linx.ano AND i.cod_un_negocio = b_linx.cod_un_negocio
+        ON i.cod_material = b_linx.cod_material 
+        AND i.ano = b_linx.ano 
+        AND i.cod_un_negocio = b_linx.cod_un_negocio
     ),
     base_cpv_combos AS (
       SELECT
+        cpvs.ano,
         combo.cod_material_pai AS cod_material,
-        combo.cod_un_negocio,
+        combo.cod_un_negocio,        
         SUM(cpvs.cpv) AS cpv
       FROM tb_produto_sku_combo AS combo
-      LEFT JOIN base_cpvs_consolidados AS cpvs
-        ON combo.cod_un_negocio = cpvs.cod_un_negocio AND combo.cod_material_filho = cpvs.cod_material
-      GROUP BY
-        1, 2
+      INNER JOIN base_cpvs_consolidados AS cpvs
+        ON combo.cod_un_negocio = cpvs.cod_un_negocio 
+        AND combo.cod_material_filho = cpvs.cod_material
+      GROUP BY 1, 2, 3
     )
     SELECT
       cpvs.ano,
@@ -64,8 +74,11 @@ BEGIN
       IF(skus.flg_combo IS TRUE, combo.cpv, cpvs.cpv) AS cpv
     FROM base_cpvs_consolidados AS cpvs
     INNER JOIN tb_produto_skus AS skus
-      ON cpvs.cod_un_negocio = skus.cod_un_negocio AND cpvs.cod_material = skus.cod_material
+      ON cpvs.cod_un_negocio = skus.cod_un_negocio 
+      AND cpvs.cod_material = skus.cod_material
     LEFT JOIN base_cpv_combos AS combo
-      ON cpvs.cod_un_negocio = combo.cod_un_negocio AND cpvs.cod_material = combo.cod_material
+      ON cpvs.cod_un_negocio = combo.cod_un_negocio 
+      AND cpvs.cod_material = combo.cod_material
+      AND cpvs.ano = combo.ano
   );
 END;
